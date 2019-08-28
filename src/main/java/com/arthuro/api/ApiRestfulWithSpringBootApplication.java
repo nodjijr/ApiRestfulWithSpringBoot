2fc29d0edd2f4b4ba8d6ccb000913afd26de4a65
nodji <nodji.junior@senior.com.br>
2019-08-28 19:16:15 -0300

    Initial version

---
+++ src/main/java/com/arthuro/api/ApiRestfulWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.arthuro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulWithSpringBootApplication.class, args);
+	}
+
+}
